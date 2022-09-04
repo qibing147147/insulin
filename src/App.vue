8fd20ac2f9779d89811b753ec0c1c5431b48b5e9
qibing147147 <qibing147147@163.com>
2022-09-04 17:45:21 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <tree :data="treeData" />
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import tree from './components/index.vue'
+import axios from 'axios'
+import { reactive } from 'vue';
+
+const treeData = reactive([])
+
+axios.get('./data.json').then(res => {
+  treeData.push(res.data)
+})
+
+
+
+</script>
+  
+<style scoped>
+
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="scroller" @scroll="handleScroll">
+    <div class="container" :style="{ paddingTop: `${scrollTop}px` }">
+      <div v-for="item in treeData" :key="item.id" class="item" :style="{paddingLeft: item.level * 20 + 'px'}">
+        {{ item.id }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, reactive, ref } from 'vue';
+
+defineProps<{
+  data: any
+}>()
+
+const worker = new Worker('worker.js')
+let now = Date.now()
+let i = 1
+
+let data: any[] = []
+// let treeData = reactive([])
+let scrollTop = ref(0)
+
+onMounted(() => {
+  setData()
+})
+
+worker.onmessage = (e) => {
+  treeData.length = 0
+  if (typeof e.data === 'string') {
+    data = JSON.parse(e.data)
+  } else {
+    data = JSON.parse(e.data.data)
+    console.log('onmessage', Date.now() - now)
+    console.log('onmessage startIndex', e.data.startIndex)
+    console.log('onmessage i', i)
+    i += 1
+  }
+  treeData.push(...data)
+}
+
+function setData () {
+  worker.postMessage({
+    type: 'init'
+  })
+}
+function flat (data, level = 1) {
+  const res = []
+  const len = data.length
+
+  for (let i = 0; i < len; i++) {
+    const cur = data[i]
+    res.push({
+      ...cur,
+      level
+    })
+    if (cur.children) {
+      res.push(...flat(cur.children, level + 1))
+    }
+  }
+  return res
+}
+
+function handleScroll (e: UIEvent) {
+  e.preventDefault()
+  const startIndex = Math.floor(e.target!.scrollTop / 25)
+  console.log('startIndex', startIndex)
+  if (now) {
+    console.log('scroll', Date.now() - now)
+  }
+  console.log('scroll i', i)
+  now = Date.now()
+  worker.postMessage({
+    startIndex
+  })
+  scrollTop.value = e.target!.scrollTop
+  // this.treeData = this.data.slice(startIndex, startIndex + 50)
+}
+</script>
+
+<style lang="scss" scoped>
+.scroller {
+  height: 100%;
+  overflow: auto;
+}
+
+.container {
+  height: 25000000px;
+}
+
+.item {
+  height: 25px;
+  line-height: 25px;
+}
+</style>
